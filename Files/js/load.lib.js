a339e87b6428941fd3bdae05dc50d7fa1414b995
hiropen <hiropen1414@gmail.com>
2022-10-16 13:48:24 +0900

    メディア仮完成

--- Files/js/load.lib.js
+++ Files/js/load.lib.js
@@ -63,50 +63,41 @@ window.DATA = [
       content: "未定",
       pages: "3",
       author: "稲葉浩晃",
-      link: "hinaba_g",
       id: "3"
     }, {
       content: "作品紹介",
       pages: 5,
       author: "松崎雅巳",
-      link: "mmatuzaki",
       id: "4"
     }, {
       content: "自己紹介,モデリング",
       pages: 6,
       author: "市橋沙夜",
-      link: "sitihasi",
       id: "5"
     }, {
       content: "自作ゲーム「Quadline」",
       pages: 7,
       author: "浅野文哉",
-      link: "hasano",
       id: "6"
     }, {
       content: "Discord Bot",
       pages: "8",
       author: "関口直生",
-      link: "nsekiguti",
       id: "7"
     }, {
       content: "自己紹介",
       pages: "8",
       author: "長澤浩平",
-      link: "knagasawa",
       id: "8"
     }, {
       content: "Chromeの拡張機能",
       pages: "8",
       author: "鹿野泰生",
-      link: "tsikano"
-      ,
       id: "9"
     }, {
       content: "自己紹介",
       pages: "8",
       author: "山中晴斗",
-      link: "hyamanaka",
       id: "10"
     }
   ],
@@ -115,25 +106,21 @@ window.DATA = [
       content: "編集モロモロをポロポロと",
       pages: "3",
       author: "倉本真菜",
-      link: "nkuramoto",
       id: "12"
     }, {
       content: "自己紹介、イラストレーターにインタビュー",
       pages: 5,
       author: "山本果歩",
-      link: "kyamamoto",
       id: "13"
     }, {
       content: "自己紹介",
       pages: 6,
       author: "三戸祐夏",
-      link: "ysando",
       id: "14"
     }, {
       content: "自己紹介",
       pages: 7,
       author: "渡辺颯太",
-      link: "swatanabe",
       id: "15"
     }
   ],
@@ -142,7 +129,6 @@ window.DATA = [
       content: "WebComponentについて",
       pages: "3~4",
       author: "稲葉浩晃",
-      link: "hinaba_w",
       id: "17"
     }
   ],
@@ -151,8 +137,6 @@ window.DATA = [
       content: "活動実績",
       pages: "3~4",
       author: "高須俊輔",
-      link: "stakasu"
-      ,
       id: "19"
     }
   ]
